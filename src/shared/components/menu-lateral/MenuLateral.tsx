874c7722b82b650bf5bf79c8c850673447a189eb
Jefferson Lessa <jefferson2459@gmail.com>
2024-03-19 18:18:51 -0300

    acerto para Erro de API config

---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,74 @@
+
+import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from "@mui/material"
+import { useDrawerContext } from "../../contexts";
+import React from "react";
+import { useMatch, useNavigate, useResolvedPath } from "react-router-dom";
+
+interface IMenuListItemProps {
+    to: string;
+    icon: string;
+    label: string;
+    onClick: (() => void) | undefined;
+}
+
+interface IMenuLateralProps{
+    children: React.ReactNode;
+}
+
+const MenuListItem: React.FC<IMenuListItemProps> = ({ to, icon, label, onClick }) => {
+    const navigate = useNavigate();
+
+    const resolvedPath = useResolvedPath(to);
+    const match = useMatch({ path: resolvedPath.pathname, end: false });
+
+    const handleClick = () => {
+        navigate(to);
+        onClick?.();
+    }
+
+    return (
+        <ListItemButton selected={!!match} onClick={handleClick}>
+            <ListItemIcon>
+                <Icon>{icon}</Icon>
+            </ListItemIcon>
+            <ListItemText primary={label} />
+        </ListItemButton>
+    )
+}
+
+export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
+    const theme = useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+    const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
+    return (
+        <>
+            <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
+                <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+                    <Box width="100%" height={theme.spacing(16)} display="flex" alignItems="center" justifyContent={"center"}>
+                        <Avatar
+                            sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+                            src="https://media.licdn.com/dms/image/C4E0BAQFP8m6_99qI1w/company-logo_200_200/0/1630634079552?e=2147483647&v=beta&t=UXUSTvCQMvW1jyt4X_N6vjUp_IaZxAM8kYhK1_KYnkU" />
+                    </Box>
+                    <Divider />
+                    <Box flex={1}>
+                        <List component="nav">
+                            {drawerOptions.map(drawerOption => (
+                                <MenuListItem
+                                    key={drawerOption.path}
+                                    to={drawerOption.path}
+                                    icon={drawerOption.icon}
+                                    label={drawerOption.label}
+                                    onClick={smDown ? toggleDrawerOpen : undefined}
+                                />
+                            ))}
+                        </List>
+                    </Box>
+
+                </Box>
+            </Drawer>
+            <Box height="100vh" marginLeft={theme.spacing(28)}>
+                {children}
+            </Box>
+        </>
+    )
+};
--- src/shared/services/api/axios-config/index.ts
+++ src/shared/services/api/axios-config/index.ts
@@ -3,7 +3,7 @@ import { Environment } from "../../../environment";
 
 export const Api = axios.create({
     baseURL: Environment.BASE_API,
-    // headers: {
-    //     Authorization: `Bearer ${JSON.parse(localStorage.getItem(Environment.TOKEN_STRING) || "")}`
-    // }
+    headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem(Environment.TOKEN_STRING ) || '{}')}`
+    }
 });
